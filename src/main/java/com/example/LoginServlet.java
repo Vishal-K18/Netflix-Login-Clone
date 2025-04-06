f2088b8dabbfe90ce60d2b95767eaa14195060b5
Vishal Kumar <147913826+Vishal-K18@users.noreply.github.com>
2025-04-06 22:04:17 +0530

    Initial commit

---
+++ src/main/java/com/example/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            // Load JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Connect to DB
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/login", "root", "Vishal@624");
+
+            // Insert user into DB
+            String sql = "INSERT INTO userdata (username, password) VALUES (?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            int row = stmt.executeUpdate();
+            if (row > 0) {
+            	response.sendRedirect("welcome.jsp");
+            } else {
+                response.getWriter().println("Login failed");
+            }
+
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("Error: " + e.getMessage());
+        }
+    }
+}
