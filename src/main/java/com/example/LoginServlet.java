bb8fb2034820ed3f17299143f055b35680b08641
Vishal Kumar <147913826+Vishal-K18@users.noreply.github.com>
2025-04-09 18:57:54 +0530

    new root.war

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
